80d7172a153b1f4e4668c0edbbcdadee601f3ca5
Carlos Kimura <carloskimura@Carloss-MacBook-Pro.local>
2022-12-06 22:30:49 -0300

    initial project files added with divkit package included

---
+++ Pets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pets
+//
+//  Created by Carlos Kimura on 06/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
